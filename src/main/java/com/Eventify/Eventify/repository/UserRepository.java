f8af3b988793894a84fc096620fe989972fbd818
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-18 02:42:21 +0100

    here i add the repo of user + custom methodes

---
+++ src/main/java/com/Eventify/Eventify/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.Eventify.Eventify.repository;
+
+
+import com.Eventify.Eventify.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
